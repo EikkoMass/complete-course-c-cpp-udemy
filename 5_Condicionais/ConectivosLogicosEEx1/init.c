dd4b731dd5dd37d024baf4c8f630f6858084a7e2
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-04-11 19:57:41 -0300

    Conectivos Lógicos E - EX1

---
+++ 5_Condicionais/ConectivosLogicosEEx1/init.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+  int a;
+
+  scanf(" %d", &a);
+
+  if(a > 0 && a < 10)
+  {
+    printf("%d esta entre 0 e 10\n");
+  }
+
+  return 0;
+}
